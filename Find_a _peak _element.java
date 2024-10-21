dc70470b3cad49e214c93d2541f282f4e209dc35
varun645 <64206531+varun645@users.noreply.github.com>
2024-10-21 20:22:29 +0530

    Update and rename Find a peak element to Find_a _peak _element.java

--- Find_a _peak _element.java
+++ Find_a _peak _element.java
@@ -1,3 +1,4 @@
+/**
 Given an 0-indexed array of integers arr[] of size n, find its peak element and return it's index. An element is considered to be peak if it's value is greater than or equal to the values of its adjacent elements (if they exist).
 
 Note: The output will be 1 if the index returned by your function is correct; otherwise, it will be 0.
@@ -17,7 +18,7 @@ Constraints:
 1 ≤ n ≤ 105
 1 ≤ arr[i] ≤ 106
 
-
+**/
 
 
 
