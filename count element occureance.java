0420cdb0c92d67094ebd7212869c048fd260eac2
varun645 <64206531+varun645@users.noreply.github.com>
2024-11-02 11:23:35 +0530

    Create count element occureance.java

---
+++ count element occureance.java
@@ -0,0 +1,58 @@
+/**
+Given an array arr of size N and an element k. The task is to find the count of elements in the array that appear more than n/k times.
+
+Example 1:
+
+Input:
+N = 8
+arr = [3,1,2,2,1,2,3,3]
+k = 4
+Output: 
+2
+Explanation: 
+In the given array, 3 and 2 are the only elements that appears more than n/k times.
+Example 2:
+
+Input:
+N = 4
+arr = [2,3,3,2]
+k = 3
+Output: 
+2
+Explanation: In the given array, 3 and 2 are the only elements that appears more than n/k times. So the count of elements are 2.
+Constraints:
+1 <= N <= 105
+0 <= a[i] <= 109
+1 <= k <= N
+
+
+
+**/
+
+
+class Solution 
+{
+    //Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        int x = n/k ; 
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int num : arr){
+            if(map.containsKey(num)){
+               map.put(num , map.get(num) + 1 ); 
+            }else{
+                map.put(num , 1);
+            }
+            
+        }
+        
+        int count = 0 ; 
+        for(int num : map.keySet()){
+            if(map.get(num) > x){
+                count++ ; 
+            }
+        }
+        
+        return count;
+    }
+}
